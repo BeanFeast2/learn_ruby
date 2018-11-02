db200afb31a754f1aef36d434332aef6ef345c85
Vince Carollo <vincecarollo@gmail.com>
2018-11-02 16:35:28 -0600

    finished timers assignment, all pass

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
-  #write your code here
+  attr_reader :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @minutes -= @hours * 60
+    @seconds -= @minutes * 60 
+    @seconds -= @hours * 3600
+    "#{@hours.to_s.rjust(2,'0')}:#{@minutes.to_s.rjust(2,'0')}:#{@seconds.to_s.rjust(2,'0')}"
+  end
+
 end
+
+a = Timer.new
+a.seconds = 12
+puts a.time_string
