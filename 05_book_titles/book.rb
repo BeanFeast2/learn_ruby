6360909bf0fabb04bb98ba2be012182f6447bab5
Vince Carollo <vincecarollo@gmail.com>
2018-11-02 15:10:34 -0600

    finished book assignment, all pass

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  attr_reader :title
+  @@no_cap_words = ['in','the','a','of','and','an']
+  def title=(book_title)
+    book_title =  book_title.split(' ')
+    book_title.map! do |word|
+      if !@@no_cap_words.include?(word) || word == book_title[0]
+        #if word is number don't capitalize
+        if word == word.to_i.to_s
+          word
+        else
+          word.capitalize!
+        end
+      else
+        word
+      end
+    end
+    @title = book_title.join(' ')
+  end
 end
